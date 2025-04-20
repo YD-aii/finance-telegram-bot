1040d17fd3986408a5c9816ecbe32b5b28be99c5
YD-aii <y.d.y.d.y.d@yandex.ru>
2025-04-20 18:01:01 +0400

    Fix import paths to config.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ConversationHandler
 import database as db
-from config import BOT_TOKEN
+from last_version.config import BOT_TOKEN
+
 from database import is_user_registered, register_user
 from config import AUTH_CODE
 from config import ADMIN_IDS
